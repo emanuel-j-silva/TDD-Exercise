e08123a70cdb1196e447f831769b9380492b33a4
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-24 18:06:51 -0300

    feat: main demo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input1 = "2 2 2 2 2 2 3";
+        String input2 = "21 10 13 34 30 21 34";
+        String input3 = "2 2 2 2 2 2 1";
+        String input4 = "2 # 2 2 T 2 1";
+
+        WeekAverage example1 = new WeekAverage();
+        WeekAverage example2 = new WeekAverage();
+        WeekAverage example3 = new WeekAverage();
+        WeekAverage example4 = new WeekAverage();
+
+        System.out.println(example1.daysAbove(input1));
+        System.out.println(example2.daysAbove(input2));
+        System.out.println(example3.daysAbove(input3));
+        System.out.println(example4.daysAbove(input4));
+
+
     }
 }

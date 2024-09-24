805bfdd00c785c7c1fb49a3a833781fe9b828ce4
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-24 00:48:08 -0300

    test: calculation with only zero in the input

--- src/test/java/org/example/WeekAverageTest.java
+++ src/test/java/org/example/WeekAverageTest.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 public class WeekAverageTest {
+
+    @Test
+    @DisplayName("Should return zero on input")
+    void shouldReturnZero(){
+        WeekAverage weekAverage = new WeekAverage();
+        String input = "0 0 0 0 0 0 0";
+
+        assertThat(weekAverage.daysAbove(input)).isEqualTo(0);
+    }
 }

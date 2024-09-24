d57d0ae1a795bcd20335bfdc8050226c112aa23e
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-24 01:34:37 -0300

    refactor: increase logic to calculate days above average

--- src/main/java/org/example/WeekAverage.java
+++ src/main/java/org/example/WeekAverage.java
@@ -1,8 +1,33 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeekAverage {
 
     public int daysAbove(String input){
-        return 0;
+        int daysAbove = 0;
+        double avg = 0;
+        List<Integer> temperatures = new ArrayList<>();
+        String[] values = input.split(" ");
+
+        for(String str:values){
+            Integer temperature = Integer.parseInt(str);
+            temperatures.add(temperature);
+        }
+
+        for(Integer temperature:temperatures){
+            avg += temperature;
+        }
+
+        if (avg > 0) avg = avg / temperatures.size();
+
+        for (Integer temperature:temperatures){
+            if (temperature > avg){
+                daysAbove++;
+            }
+        }
+
+        return daysAbove;
     }
 }

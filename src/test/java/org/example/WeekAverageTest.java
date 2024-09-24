7505b29f79f10da20a909b41a41629894c01b14f
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-24 01:42:51 -0300

    test: reduce code redundancy

--- src/test/java/org/example/WeekAverageTest.java
+++ src/test/java/org/example/WeekAverageTest.java
@@ -1,16 +1,24 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 public class WeekAverageTest {
 
+    private WeekAverage weekAverage;
+    private String input;
+
+    @BeforeEach
+    void setUp(){
+        weekAverage = new WeekAverage();
+    }
+
     @Test
     @DisplayName("Should return zero on input")
     void shouldReturnZero(){
-        WeekAverage weekAverage = new WeekAverage();
-        String input = "0 0 0 0 0 0 0";
+        input = "0 0 0 0 0 0 0";
 
         assertThat(weekAverage.daysAbove(input)).isEqualTo(0);
     }
@@ -18,8 +26,7 @@ public class WeekAverageTest {
     @Test
     @DisplayName("Should return one in days above average calculation")
     void shouldReturnOne(){
-       WeekAverage weekAverage = new WeekAverage();
-       String input = "2 2 2 2 2 2 2 3";
+        input = "2 2 2 2 2 2 2 3";
 
        assertThat(weekAverage.daysAbove(input)).isEqualTo(1);
     }

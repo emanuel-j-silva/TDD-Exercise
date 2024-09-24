a35a66c5da1f9b7e0595421dccb81959670ffe72
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-24 18:25:55 -0300

    refactor: simplify main demo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,12 @@ public class Main {
         String input3 = "2 2 2 2 2 2 1";
         String input4 = "2 # 2 2 T 2 1";
 
-        WeekAverage example1 = new WeekAverage();
-        WeekAverage example2 = new WeekAverage();
-        WeekAverage example3 = new WeekAverage();
-        WeekAverage example4 = new WeekAverage();
+        WeekAverage weekAverage = new WeekAverage();
 
-        System.out.println(example1.daysAbove(input1));
-        System.out.println(example2.daysAbove(input2));
-        System.out.println(example3.daysAbove(input3));
-        System.out.println(example4.daysAbove(input4));
+        System.out.println(weekAverage.daysAbove(input1));
+        System.out.println(weekAverage.daysAbove(input2));
+        System.out.println(weekAverage.daysAbove(input3));
+        System.out.println(weekAverage.daysAbove(input4));
 
 
     }
